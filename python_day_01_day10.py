f87f8b602a0d3f66514c38d51ff0160c60cad26b
Ritresh <ritresh9@gmail.com>
2026-04-04 10:00:00 +0530

    day 3

--- python_day_01_day10.py
+++ python_day_01_day10.py
@@ -1,10 +1,8 @@
 #Day 01- Say "Hello, World!" With Python
 # Read a full line of input from stdin and save it to our dynamically typed variable, input_string.
 input_string = input()
-
 # Print a string literal saying "Hello, World." to stdout.
 print('Hello, World.')
-
 # TODO: Write a line of code here that prints the contents of input_string to stdout.
 print(input_string)
 
@@ -27,5 +25,28 @@ print(d + d2)
 # Concatenate and print the String variables on a new line
 # The 's' variable above should be printed first.
 print(s + s2)
+
+# Day 03: Operators
+import math
+import os
+import random
+import re
+import sys
+# Complete the 'solve' function below.
+# The function accepts following parameters:
+#  1. DOUBLE meal_cost
+#  2. INTEGER tip_percent
+#  3. INTEGER tax_percent
+def solve(meal_cost, tip_percent, tax_percent):
+    # Write your code here
+    tip = meal_cost*tip_percent/100
+    tax = meal_cost*tax_percent/100
+    total_cost = meal_cost+tip+tax
+    print(round(total_cost))
+if __name__ == '__main__':
+    meal_cost = float(input().strip())
+    tip_percent = int(input().strip())
+    tax_percent = int(input().strip())
+    solve(meal_cost, tip_percent, tax_percent)
 
 
