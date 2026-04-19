f7032e813b95a695f07bf80c3ce1568d101b346f
Ritresh <ritresh9@gmail.com>
2026-04-19 19:51:22 +0530

    day-14

--- python_day_11_to_day_20.py
+++ python_day_11_to_day_20.py
@@ -100,3 +100,18 @@ author=input()
 price=int(input())
 new_novel=MyBook(title,author,price)
 new_novel.display()
+
+#Day 14: Scope
+class Difference:
+    def __init__(self, a):
+        self.__elements = a
+
+	# Add your code here
+    def computeDifference(self):
+        self.maximumDifference = max(self.__elements) - min(self.__elements)
+# End of Difference class
+_ = input()
+a = [int(e) for e in input().split(' ')]
+d = Difference(a)
+d.computeDifference()
+print(d.maximumDifference)
